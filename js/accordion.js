a9f76cee70144235d4bfe49a0a3f34fde1686096
raajs0n <adam.raj27@gmail.com>
2020-09-02 20:12:04 +0200

    Weryfikacja

--- js/accordion.js
+++ js/accordion.js
@@ -8,10 +8,6 @@ for (i = 0; i < acc.length; i++) {
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
+    panel.classList.toggle("active");
   });
 }
