1a1511c2c08729302cacc6a374fdabd439384b72
szymonesq <114394145+szymonesq@users.noreply.github.com>
2022-09-29 17:09:41 +0200

    Add files via upload

---
+++ src/content/partners2022.js
@@ -0,0 +1,22 @@
+export const partners2022 = [
+    {
+        src: "/images/partners2022/Partnerzy-01.png",
+        href: "https://www.kearney.com/"
+    },
+    {
+        src: "/images/partners2022/Partnerzy-02.png",
+        href: "https://zeb-consulting.com/en-DE/poland"
+    },
+    {
+        src: "/images/partners2022/Partnerzy-03.png",
+        href: "https://www.bain.com/"
+    },
+    {
+        src: "/images/partners2022/Partnerzy-04.png",
+        href: "https://www.cloudity.pl/"
+    },
+    {
+        src: "/images/partners2022/Partnerzy-05.png",
+        href: "https://www.occstrategy.com/"
+    }
+]
